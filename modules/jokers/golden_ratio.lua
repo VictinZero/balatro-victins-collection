a45fd17861acea99851f7101703d97ba6e90cfd9
VictinZero <79382302+VictinZero@users.noreply.github.com>
2025-04-16 22:28:03 +0200

    Create Golden Ratio

---
+++ modules/jokers/golden_ratio.lua
@@ -0,0 +1,16 @@
+return {
+    key = 'golden_ratio',
+    config = {},
+    rarity = 1,
+    pos = { x = 7, y = 0 },
+    atlas = 'joker_atlas',
+    cost = 1,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+    soul_pos = nil,
+
+    calculate = function(self, card, context)
+    end,
+}
